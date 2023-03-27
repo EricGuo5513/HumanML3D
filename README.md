39513ba528bee03b2f40cb26e0497dcafb6e28d9
urw7rs <urw.7rs@icloud.com>
2023-03-27 20:54:58 +0900

    add empty package with version

---
+++ README.md
@@ -0,0 +1,3 @@
+#HumanML3D: 3D Human Motion-Language Dataset
+
+This repository is a refactored fork of the original [HumanML3D](https://github.com/EricGuo5513/HumanML3D.git) code. It is refactored into a python package so it can be easily integrated into any project.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="humanml3d",
+    version="0.0.0",
+    description="HumanML3D preprocessing and visualization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Chanhyuk Jung",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=["numpy"],
+    extras_require={
+        "test": ["pytest"],
+        "dev": ["black", "flake8", "bumpver"],
+    },
+)
