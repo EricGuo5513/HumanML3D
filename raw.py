160f692bf798e5a1459312c77dc3aebe39ae249c
urw7rs <urw.7rs@icloud.com>
2023-03-30 17:36:45 +0900

    separate into function for multiprocessing

--- raw.py
+++ raw.py
@@ -4,13 +4,13 @@ import os.path as osp
 from tqdm import tqdm
 
 import numpy as np
-
 import torch
-
 from einops import rearrange, repeat
 
+import pandas as pd
+
 from humanml3d.body_models import BodyModel
-from humanml3d.utils.extract import extract_smpl_files, extract_zip_files
+from humanml3d.utils import extract_smpl_files, extract_zip_files, add_root
 
 comp_device = torch.device("cuda:0") if torch.cuda.is_available() else "cpu"
 
@@ -125,12 +125,72 @@ def process_raw(amass_root, smpl_root, save_root):
         amass_to_pose(path, save_path, male_bm, female_bm)
 
 
+def swap_left_right(data):
+    assert len(data.shape) == 3 and data.shape[-1] == 3
+    data = data.copy()
+    data[..., 0] *= -1
+    right_chain = [2, 5, 8, 11, 14, 17, 19, 21]
+    left_chain = [1, 4, 7, 10, 13, 16, 18, 20]
+    left_hand_chain = [22, 23, 24, 34, 35, 36, 25, 26, 27, 31, 32, 33, 28, 29, 30]
+    right_hand_chain = [43, 44, 45, 46, 47, 48, 40, 41, 42, 37, 38, 39, 49, 50, 51]
+    tmp = data[:, right_chain]
+    data[:, right_chain] = data[:, left_chain]
+    data[:, left_chain] = tmp
+    if data.shape[1] > 24:
+        tmp = data[:, right_hand_chain]
+        data[:, right_hand_chain] = data[:, left_hand_chain]
+        data[:, left_hand_chain] = tmp
+    return data
+
+
+def save_raw(source_path, new_name, start_frame, end_frame, fps, save_dir):
+    data = np.load(source_path)
+    if "humanact12" not in source_path:
+        if "Eyes_Japan_Dataset" in source_path:
+            data = data[3 * fps :]
+        if "MPI_HDM05" in source_path:
+            data = data[3 * fps :]
+        if "TotalCapture" in source_path:
+            data = data[1 * fps :]
+        if "MPI_Limits" in source_path:
+            data = data[1 * fps :]
+        if "Transitions_mocap" in source_path:
+            data = data[int(0.5 * fps) :]
+        data = data[start_frame:end_frame]
+        data[..., 0] *= -1
+
+    data_m = swap_left_right(data)
+    #     save_path = pjoin(save_dir, )
+    os.makedirs(save_dir, exist_ok=True)
+    np.save(osp.join(save_dir, new_name), data)
+    np.save(osp.join(save_dir, "M" + new_name), data_m)
+
+
+def segment_mirror_and_relocate(save_root, index_path="index.csv", save_dir="joints"):
+    index_file = pd.read_csv(index_path)
+    total_amount = index_file.shape[0]
+    fps = 20
+
+    for i in tqdm(range(total_amount)):
+        source_path = index_file.loc[i]["source_path"]
+        source_path = osp.normpath(source_path)
+        source_path = source_path.replace("pose_data", save_root)
+
+        new_name = index_file.loc[i]["new_name"]
+        start_frame = index_file.loc[i]["start_frame"]
+        end_frame = index_file.loc[i]["end_frame"]
+
+        save_raw(source_path, new_name, start_frame, end_frame, fps, save_dir)
+
+
 if __name__ == "__main__":
     data_dir = "amass"
     amass_root = "/data/humanml3d/amass_root"
     smpl_root = "/data/humanml3d/body_models"
     save_root = "/data/humanml3d/pose_data"
 
-    extract_files(data_dir, amass_root, smpl_root, workers=None)
+    # extract_files(data_dir, amass_root, smpl_root, workers=0)
 
-    process_raw(amass_root, smpl_root, save_root)
+    # process_raw(amass_root, smpl_root, save_root)
+
+    segment_mirror_and_relocate(save_root, "index.csv", "/data/humanml3d/joints")
